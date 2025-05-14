a1495710451723a9b9bf1a97a2c1fe90420de9be
Vladozi <programming304@gmail.com>
2025-05-14 21:32:18 +0400

    initial commit

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
+const path = require('path'); 
 
+app.use(express.static(path.join(__dirname, 'assets')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -94,6 +99,10 @@ app.post('/api/contact', async (req, res) => {
       message: 'შეცდომა გაგზავნისას' 
     });
   }
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
