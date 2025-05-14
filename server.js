a39aa84b2a3b05b554532128465e2adc8c39555c
Vladozi <programming304@gmail.com>
2025-05-14 21:37:43 +0400

    initial commit

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ const nodemailer = require('nodemailer');
 const cors = require('cors');
 const path = require('path'); 
 
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
 app.use(express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
 
 // Configure SMTP using environment variables
 const transporter = nodemailer.createTransport({
